108b8ccfd564ed3113aff4b9f910b7ac1e1820a7
Egor <yegeraskin13@gmail.com>
2019-11-27 18:07:03 +0400

    third commit

--- Calculate.java
+++ Calculate.java
@@ -1,9 +1,34 @@
 public class Calculate {
+  private final int first;
+  private final int second;
+  
   public static void main(String[] args) {
-    System.out.println("Calculate...");
-    int first = Integer.valueOf(args[0]);
-    int second = Integer.valueOf(args[1]);
-    int summ = first + second;
-    System.out.println("Sum: " + summ);
+    Calculate calc = new Calculate(Integer.valueOf(args[0]), Integer.valueOf(args[1]));
+    System.out.print(String.format("Sum: %d\nMultiply: %d\nPow: %d\nDivision: %f\n", calc.sum(), calc.multiply(), calc.pow(), calc.division()));
+  }
+  
+  public Calculate(int first, int second) {
+    this.first = first;
+    this.second = second;
+  }
+  
+  public int sum() {
+    return first + second;
+  }
+  
+  public int multiply() {
+    return first * second;
+  }
+  
+  public int pow() {
+    int result = 1;
+    for(int i = 0; i < second; ++i) {
+      result *= first;
+    }
+    return result;
+  }
+  
+  public double division() {
+    return (double)first / second;
   }
 }
