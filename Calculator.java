a489e543816116cde9c03c6508a5007c8fc3f264
Egor <yegeraskin13@gmail.com>
2019-12-07 21:20:24 +0400

    07/12/2019 (1)

--- Calculator.java
+++ Calculator.java
@@ -2,32 +2,56 @@
     Класс реализует калькулятор
 */
 public class Calculator {	
-  /** 
-    Результат вычисления
-  */
   private int result = 0;
   
-  /** 
-    Суммируем аргументы
-    @param params Аргументы суммирования
-  */
-  public void add(int ... params) {
-    for(int param : params) {
-        this.result += param;
+  public void add(int a, int b) {
+    this.result = a + b;
+  }
+  
+  public void add(int a) {
+    this.add(this.result, a);
+  }
+  
+  public void sub(int a, int b) {
+    this.result = a - b;
+  }
+  
+  public void sub(int  a) {
+    this.sub(this.result, a);
+  }
+  
+  public void multiply(int a, int b) {
+    this.result = a * b;
+  }
+  
+  public void multiply(int a) {
+    this.multiply(this.result, a);
+  }
+  
+  public void div(int a, int b) {
+    this.result = a / b;
+  }
+  
+  public void div(int a) {
+    this.div(this.result, a);
+  }
+  
+  public void pow(int base, int exponent) {
+    this.result = 1;
+    for(int i = 0; i < exponent; ++i) {
+        this.result *= base;
     }
   }
   
-  /** 
-    Возвращаем результат суммирования
-    @return результат суммирования
-  */
-  public int getResult() {
-    return this.result;
+  public void pow(int exponent) {
+    this.pow(this.result, exponent);
   }
-  /** 
-    Очистить результат вычисления
-  */
+  
   public void cleanResult() {
     this.result = 0;
+  }
+  
+  public int getResult() {
+    return result;
   }
 }
--- InteractRunner.java
+++ InteractRunner.java
@@ -3,22 +3,97 @@ import java.util.Scanner;
 public class InteractRunner {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    String exit = "no";
+    Calculator calc = new Calculator();
     try {
-      String finishStatus = "no";
-      Calculator calc = new Calculator();
-      while(!finishStatus.equals("yes")) {     
-        System.out.println("Enter first arg:");
-        int first = Integer.parseInt(sc.next());
-        System.out.println("Enter second arg:");
-        int second = Integer.parseInt(sc.next());
-        calc.add(first, second);
-        System.out.printf("Sum is: %d\nShould we finish? Enter yes/no\n", calc.getResult());
-        calc.cleanResult();
-        finishStatus = sc.next();
-      }
-      
+        while(!exit.equals("yes")) {
+            String operation;
+            do {
+                System.out.println("Please, enter correct operation(+, -, *, /, ^):");
+                operation = sc.nextLine();
+            } while(!correctOperation(operation));
+            String arguments;
+            do {
+                System.out.println("Enter 1 or 2 arguments separated with space");
+                arguments = sc.nextLine();
+            } while(!correctArguments(arguments));
+            String[] argss = arguments.split(" ");
+            
+            int first = Integer.parseInt(argss[0]);
+            int second = 0;
+            if (argss.length == 2) {
+                second = Integer.parseInt(argss[1]);
+            }
+            
+            if (operation.equals("+")) {
+                if (argss.length == 1) {
+                    calc.add(first);
+                } else {
+                    calc.add(first, second);
+                }
+            }
+            
+            if (operation.equals("-")) {
+                if (argss.length == 1) {
+                    calc.sub(first);
+                } else {
+                    calc.sub(first, second);
+                }
+            }
+            
+            if (operation.equals("*")) {
+                if (argss.length == 1) {
+                    calc.multiply(first);
+                } else {
+                    calc.multiply(first, second);
+                }
+            }
+            
+            if (operation.equals("/")) {
+                if (argss.length == 1) {
+                    calc.div(first);
+                } else {
+                    calc.div(first, second);
+                }
+            }
+            
+            if (operation.equals("^")) {
+                if (argss.length == 1) {
+                    calc.pow(first);
+                } else {
+                    calc.pow(first, second);
+                }
+            }
+            
+            System.out.println(String.format("Result of the operation: %d", calc.getResult()));
+            
+            System.out.println("Should we finish? Enter yes/no");
+            exit = sc.nextLine();
+        }
     } finally {
-      sc.close();
+        sc.close();
     }
+  }
+  
+  private static boolean correctOperation(String operation) {
+    return operation.equals("+") || operation.equals("-") || operation.equals("*") ||
+            operation.equals("/") || operation.equals("^");
+  }
+  
+  private static boolean correctArguments(String arguments) {
+    String [] args = arguments.split(" ");
+    if (args.length == 0 || args.length > 2) {
+        return false;
+    }
+    
+    for(String arg : args) {
+        try {
+            Integer.parseInt(arg);
+        } catch(Exception ex) {
+            return false;
+        }
+    }
+    
+    return true;
   }
 }
